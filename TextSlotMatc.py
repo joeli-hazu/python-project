afd2d9db93c27c7001f38beacba8259c1352d339
Yueil Kaleab <yueildani03@gmail.com>
2026-03-09 15:56:06 +0100

    second

--- TextSlotMatc.py
+++ TextSlotMatc.py
@@ -1,24 +1,44 @@
 balance = 0
 
+def get_menu(balance):
+        menu_text = f"""----- SLOT MACHINE -----
+        Balance: €{balance}
+        1 - Deposit
+        2 - Play
+        3 - Withdraw
+        4 - Exit"""
+
+        print(menu_text)
+        choice = int(input("What do you want to do"))
+        return choice
+
+
+    
+
 def deposit():
-    while True:
-        try:
-            amount = int(input("How much would you like to deposit? "))
+        while True:
+            try:
+                amount = int(input("How much would you like to deposit? "))
 
-            if amount <= 0:
-                print("You must deposit a number greater than 0")
-            else:
-                return amount
+                if amount <= 0:
+                    print("You must deposit a number greater than 0")
+                else:
+                        return amount
 
-        except ValueError:
-            print("Invalid input. Please enter a number")
+            except ValueError:
+                    print("Invalid input. Please enter a number")
 
 
 
 
 
 def main():
-    balance += deposit()
-    print("Your balance is:", balance)
+    global balance
+    while True:
+        choice = get_menu(balance)
+
+        if choice == "1":
+              balance += deposit()
+              print("You balance is:", balance)
 
 main()
